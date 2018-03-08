08d261128fb1caebd0a051f43e13f5dade1f77ff
SSokolan <sokolan.sergey@gmail.com>
2018-03-08 22:49:16 +0300

    megachat

--- src/components/chat/chat.js
+++ src/components/chat/chat.js
@@ -1,9 +1,50 @@
-'use strict';
+"use strict";
 
-class Chat {
-    constructor() {}
+export const DEFAULT_USER_NAME = "%username";
+
+export class Chat {
+    constructor({ el, data }) {
+        this.el = el;
+        this.data = {
+            nickName: DEFAULT_USER_NAME,
+            messages: [{
+                    nickname: "Some nickname",
+                    text: "text",
+                    data: new Date()
+                },
+                {
+                    nickname: "Another nickname",
+                    text: "another text",
+                    data: new Date()
+                }
+            ]
+        };
+    }
+
+    addMessage(messageObj) {
+        this.data.messages.push(messageObj);
+        this.render();
+    }
 
     render() {
-'some';
+        this.el.innerHTML = `
+        <div class="chat">
+            <div class="chat__messages">
+                ${this._getMessagesHtml()}
+            </div>
+        </div>
+        `;
+    }
+
+    _getMessagesHtml() {
+        return this.data.messages
+            .map(
+                messageObj => `
+                <span class="chat__message">
+                    ${messageObj.nickname}: ${messageObj.text}
+                </span>
+            `
+            )
+            .join(`<br>`);
     }
 }
